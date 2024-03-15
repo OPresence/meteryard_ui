6c6a9a46f8aad96ea3508dd6be59b8d860c5b79f
re-monu <osndl.monur@aicofindia.com>
2024-03-15 10:03:11 +0530

    update the code for merge

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "beaba1ae859fece8"; }
+/******/ 	__webpack_require__.h = function() { return "9b58a8a88cdfd6a2"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
