7692b80e52a85cc44cb74ca691a2df5bcae32021
re-monu <osndl.monur@aicofindia.com>
2024-03-18 10:41:21 +0530

    update the code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd29e94cd3521346"; }
+/******/ 		__webpack_require__.h = function() { return "6bc3d695839b53ae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
