c3a82ff0af53f2b52732292418c41d9c4bf38d88
nikita3006 <nikitanegi3011@gmail.com>
2024-04-25 20:09:07 +0530

    correcting comment section style

--- src/pages/CityChat/PostSection/PostCard.js
+++ src/pages/CityChat/PostSection/PostCard.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
+import Modal from '@mui/material/Modal';
 import {
   Divider,
   Typography,
@@ -9,6 +10,7 @@ import {
   IconButton,
 } from "@mui/material";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import { FaShare } from "react-icons/fa6";
 import { BsChatDots } from "react-icons/bs";
 import SendIcon from "@mui/icons-material/Send";
@@ -114,10 +116,23 @@ const CityPropertyStyle = styled("Box")(({ theme }) => ({
 }));
 
 const PostCard = ({ data, onLike, AddCommentFunction }) => {
+
+
+
   const [liked, setLiked] = useState(false);
   const [_addcomment, setAddComment] = useState("");
 
   const [_showcomment, setShowComment] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   console.log("mjnnkn--->", liked);
   console.log("data--->", data);
   const handleLike = () => {
@@ -132,11 +147,46 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
       <Box className="mainBox">
         <Box maxWidth={280} position={"absolute"} right={-17} top={-13}>
           <img src="/images/Path 8257.svg" width={"100%"} />
+          <Button
+            style={{
+              position: 'absolute',
+              right: 25,
+              top: 25,
+              zIndex: 2,
+              padding: 0, // Remove default button padding
+              minWidth: 0, // Ensure button doesn't affect layout
+            }}
+            onClick={handleOpen}
+          >
+            <DashboardIcon
+              style={{ color: 'black' }}
+            />
+          </Button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+          >
+            <Box sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: 400,
+              bgcolor: 'background.paper',
+              border: '2px solid #000',
+              boxShadow: 24,
+              p: 4,
+            }}>
+              {/* Content of the modal */}
+              Modal Content
+            </Box>
+          </Modal>
         </Box>
         <Box>
-          <Box className="ProfileBox">
-            <Box display={"flex"} alignItems={"center"}>
-              <Box className="imgBox">
+          <Box className="ProfileBox" >
+            <Box display={"flex"} alignItems={"center"} >
+              <Box className="imgBox" >
                 <img
                   src={
                     data?.sellerId?.profilePicture == ""
@@ -168,7 +218,7 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
           <Box>
             <img src={data?.coverImage} width={"100%"} />
           </Box>
-          <Box padding={"15px"}>
+          <Box padding={"15px"} >
             <Box
               className="commentBox"
               display={"flex"}
@@ -248,7 +298,7 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
                 >
                   <Box style={{ display: "flex", alignItems: "center" }}>
                     <BsChatDots />
-                    <Typography variant="h6"  sx={{ marginLeft: 1 }}>
+                    <Typography variant="h6" sx={{ marginLeft: 1 }}>
                       Comment
                     </Typography>
                   </Box>
@@ -256,7 +306,7 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
                 <Box style={{ display: "flex", alignItems: "center" }}>
                   <FaShare />
                   &nbsp;
-                  <Typography variant="h6"  sx={{ marginLeft: 1 }}>
+                  <Typography variant="h6" sx={{ marginLeft: 1 }}>
                     SHARE
                   </Typography>
                 </Box>
@@ -264,7 +314,7 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
             </Box>
 
             {_showcomment && (
-              <Box p={"0 40px"}>
+              <Box>
                 <Box className="comment-section">
                   <Stack direction="row" spacing={2}>
                     <Avatar alt="Remy Sharp" src="/images/1567018939360.png" />
@@ -288,24 +338,24 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
                     <SendIcon />
                   </Button>
                 </Box>
-                <Box className="comment-details">
+                <Box className="comment-details" sx={{ml:'0.75rem' }}>
                   <Stack
                     direction="row"
                     spacing={2}
                     className="comment-details-below"
                   >
                     <Avatar
-                      sx={{ width: "20px", height: "20px" }}
+                      sx={{ width: "20px", height: "20px",}}
                       alt="Remy Sharp"
                       src="/images/1567018939360.png"
                     />
                     <Typography variant="h6">ashok sharma</Typography>
                   </Stack>
-                  <Box className="sub-comment-section">
-                    <Typography sx={{ marginTop: "8px" }}>
+                  <Box className="sub-comment-section" sx={{ marginTop: "5px",  }}>
+                    <Typography >
                       <span>Good</span>
                     </Typography>
-                    <Box className="iconBox activity-icon-comments">
+                    <Box className="iconBox activity-icon-comments" sx={{ fontSize: "13px",  }}>
                       <span>8 min ago</span>
                       <FavoriteBorderIcon />
                       <EditIcon />
@@ -322,4 +372,4 @@ const PostCard = ({ data, onLike, AddCommentFunction }) => {
   );
 };
 
-export default PostCard;
+export default PostCard
