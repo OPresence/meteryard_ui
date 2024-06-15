42832a536135e22b7881d1fb95494cded88e673e
Sandhyameteryard <sandhya.meteryard@gmail.com>
2024-06-15 16:52:50 +0530

    Update ApiConfig.js

--- src/ApiConfig/ApiConfig.js
+++ src/ApiConfig/ApiConfig.js
@@ -2,11 +2,11 @@
   const API_Production = process.env.NEXT_PUBLIC_API_URL;
   const API_STAGING = process.env.NEXT_PUBLIC_SITE_URL;
   var URL_ENDPOINT="https://lms-api-backend.onrender.com/api/v1";
-  // if (process.env.NODE_ENV === 'production') {
-  //   URL_ENDPOINT = API_Production;
-  // } else {
-  //   URL_ENDPOINT = API_STAGING;
-  //   }
+  if (process.env.NODE_ENV === 'production') {
+    URL_ENDPOINT = API_Production;
+  } else {
+    URL_ENDPOINT = API_STAGING;
+    }
   const Apiconfigs = {
     userSignUp: `${URL_ENDPOINT}/user/userSignUp`,
     login: `${URL_ENDPOINT}/user/login`,
